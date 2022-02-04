8b581f4a8dc671a394ad775ba6b46887cb95d197
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-02-04 23:00:08 +0900

    + 실시간 db 세팅함

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,10 +2,27 @@ import React, { useState } from "react";
 import css from "./main.module.css";
 import BusinessCard from "./businessCard/BusinessCard";
 import { useNavigate } from "react-router-dom";
-
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { ProfileBox, ProfileBoxTemplate } from "components/main/profileBox";
 
 const Main = () => {
+  // DB 사용 준비
+  const firebaseConfig = {
+    apiKey: "AIzaSyBb71mkpUGzjcrKEViTKoV_NxBxOdUe6v8",
+    authDomain: "ellie-card-maker.firebaseapp.com",
+    databaseURL: "https://ellie-card-maker-default-rtdb.firebaseio.com",
+    projectId: "ellie-card-maker",
+    storageBucket: "ellie-card-maker.appspot.com",
+    messagingSenderId: "190507871648",
+    appId: "1:190507871648:web:2d31934116b6db82cf48c5",
+    measurementId: "G-3GPCGWW4JX",
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const database = getDatabase(app);
+
   const [profile, setProfile] = useState({
     name: "박상훈",
     company: "네이버",
